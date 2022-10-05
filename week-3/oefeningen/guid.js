d9b305a50df3e048d6336a28e071b589229373f9
Quinten Mahieu <113594516+pgm-quinmahi@users.noreply.github.com>
2022-10-05 15:14:17 +0200

    Week 3, Home, updating guid.js

--- week-3/oefeningen/guid.js
+++ week-3/oefeningen/guid.js
@@ -17,11 +17,16 @@ _____________________________`);
 function generateGUID(n) {
     let output = "";
     const takeFromHere = "abcdefghijklmnopqrstuvwxyz0123456789@#&é”’\"\'\\(§è!çà)°-_¨^*$€%ù£`<>?,;./:+="; // Characters the password generator will chose from
-    for (let i = 0; i <= n; i++) { // For loop to select the amount of characters === n
+    for (let i = 0; i <= (n - 1); i++) { // For loop to select the amount of characters === n
         let char = (Math.floor(Math.random() * 74)); // Random number selector === the amount of characters from takeFromHere
         output += takeFromHere.charAt(char); // Adds the chosen character from the Math.random to the output string
     }
     return output;
 }
 
-console.log(`${generateGUID("32")}`);
+console.log(`
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+Random password generator
+------------------------------------------------------------------------------------
+Password | ${generateGUID(10)}
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_`);
