8c750b5e995094ec1e2916d9450e9485d3b71776
Quinten Mahieu <113594516+pgm-quinmahi@users.noreply.github.com>
2022-10-05 15:11:47 +0200

    Week 3, Home, adding count_words_instring.js + guid.js

---
+++ week-3/oefeningen/count_words_in_string.js
@@ -0,0 +1,35 @@
+// Console log | before assigment
+const professor = "Michael Vanderpoorten";
+const student = "Quinten Mahieu";
+const assignment = "Count words in string | https://www.pgm.gent/pgm-1/exercises/week_3.html#count-words-in-a-string";
+
+console.log(`
+░▄▀▀░▄▀▄░█▄░█░▄▀▀░▄▀▄░█▒░▒██▀
+░▀▄▄░▀▄▀░█▒▀█▒▄██░▀▄▀▒█▄▄░█▄▄
+_____________________________
+Assignment  | ${assignment} 
+Made by     | ${student}
+Professor   | ${professor} 
+_____________________________`);
+// Variables
+const str = "I am, so, so, so f@*!ing fungry!!!!!";
+
+// Functions
+function removePunctuation(myString) { // Removing punctuation, first named calculateAmountOfWords -> name changed to log the the sentence and amount of words separately
+    myString = myString.replace(/[.,\/#!$%\^&\*;:{}=\-_~()]/g,""); // Removing all punctuation
+    return myString;
+}
+
+function calculateAmountOfWords(myString) { // Removing punctuation, first named calculateAmountOfWords -> name changed to log the the sentence and amount of words separately
+    myString = myString.replace(/[.,\/#!$%\^&\*;:{}=\-_~()]/g,""); // Removing all punctuation
+    myString = myString.split(" "); // Converting the sentence into an array, where each word will be at a separate index number separately
+    return(myString.length); // Returning the lenght in numbers
+}
+
+// Console log
+console.log(`
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+Original sentence | \t\t\t\t${str}
+Sentence without punctuation | \t\t\t${removePunctuation(str)}
+Total amount of words in the sentence | \t${calculateAmountOfWords(str)}
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_`);
---
+++ week-3/oefeningen/guid.js
@@ -0,0 +1,27 @@
+// Console log | before assigment
+const professor = "Michael Vanderpoorten";
+const student = "Quinten Mahieu";
+const assignment = "Globally unique identifier | https://www.pgm.gent/pgm-1/exercises/week_3.html#globally-unique-identifier-guid";
+
+console.log(`
+░▄▀▀░▄▀▄░█▄░█░▄▀▀░▄▀▄░█▒░▒██▀
+░▀▄▄░▀▄▀░█▒▀█▒▄██░▀▄▀▒█▄▄░█▄▄
+_____________________________
+Assignment  | ${assignment} 
+Made by     | ${student}
+Professor   | ${professor} 
+_____________________________`);
+
+
+// Functions
+function generateGUID(n) {
+    let output = "";
+    const takeFromHere = "abcdefghijklmnopqrstuvwxyz0123456789@#&é”’\"\'\\(§è!çà)°-_¨^*$€%ù£`<>?,;./:+="; // Characters the password generator will chose from
+    for (let i = 0; i <= n; i++) { // For loop to select the amount of characters === n
+        let char = (Math.floor(Math.random() * 74)); // Random number selector === the amount of characters from takeFromHere
+        output += takeFromHere.charAt(char); // Adds the chosen character from the Math.random to the output string
+    }
+    return output;
+}
+
+console.log(`${generateGUID("32")}`);
