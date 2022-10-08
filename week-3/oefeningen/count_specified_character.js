0a83e296ce5a688001c84e11e1814b1674ae4af2
Quinten Mahieu <113594516+pgm-quinmahi@users.noreply.github.com>
2022-10-08 17:25:11 +0200

    Week 3, Home, pollishing up count_specified_character

--- week-3/oefeningen/count_specified_character.js
+++ week-3/oefeningen/count_specified_character.js
@@ -3,14 +3,21 @@ const string = "Count how many times the character 'o' is in this string!";
 
 // functions
 function countSpecifiedCharacter(str, chr) {
-    let c = 1; // Counter variable
+    let c = 0; // Counter variable
     for (let i = 0; i < str.length; i++) {
         if (str[i] === chr) {
             c += 1;
         }
+// If statement determining the outcome
     }
-    return c;
+    if (c === 0) {
+        return `Your character "${chr}" was not found`;
+    } else if (c === 1) {
+        return `Your character "${chr}" was found once`;
+    } else {
+        return `Your character "${chr}" was found ${c} times`;
+    }
 }
 
 // console
-console.log(countSpecifiedCharacter(string, "c"));
+console.log(countSpecifiedCharacter(string, "w"));
--- week-3/oefeningen/md/count_specified_character.md
+++ week-3/oefeningen/md/count_specified_character.md
@@ -11,4 +11,8 @@
 - [x] Complete `function countSpecifiedCharacter(str, chr)`
   - [x] Create a counter to keep track on how many chr there are in the str
   - [x] Create a loop to loop thru the whole string and count the specified chr + add 1 extra to the counter
+  - [x] Create an If statement to give different outcomes
+    - [x] If your specified character isn't found
+    - [x] If your specified character is only found once
+    - [x] If your specified character is found multiple times 
 - [x] Console log the function
